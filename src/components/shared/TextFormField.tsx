a8a30d26c664640f42316352887464d85795965b
Prasanna Kumar <prasannasrk07@gmail.com>
2021-04-20 01:32:06 +0530

    ♻️ TextformField with required

---
+++ src/components/shared/TextFormField.tsx
@@ -0,0 +1,35 @@
+import { FC } from "react";
+
+interface TextFormFieldInput {
+  label: string;
+  required?: boolean;
+  type: string;
+  placeholder: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const TextFormField: FC<TextFormFieldInput> = ({
+  label,
+  type,
+  placeholder,
+  value,
+  required = false,
+  onChange,
+}) => (
+  <div className="mb-4 box-border">
+    <label className="text-sm text-gray-500 font-medium">
+      {label}
+      <input
+        required
+        className="block mt-2 border-2 border-gray-500 border-opacity-50 rounded w-full h-11 pl-3"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  </div>
+);
+
+export default TextFormField;
