e6a4dc356878afea322e23029a27b4bdb01202ee
Prasanna Kumar <prasannasrk07@gmail.com>
2021-04-20 01:35:43 +0530

    ✨Logout button in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
-import { BiUserCircle } from "react-icons/bi";
-import { GrAddCircle } from 'react-icons/gr';
+import { FC } from "react";
+import { GrAddCircle, GrLogout } from "react-icons/gr";
+import { noterAuth } from "../firebase";
 
-const Navbar = () => (
+const Navbar: FC = () => (
   <nav className="fixed bg-white z-40 top-0 left-0 h-16 w-full border-b-2 border-gray-700 border-opacity-25 grid grid-cols-12 gap-4 items-center px-4">
     <div className="col-span-3 flex gap-2 items-center">
-      <img className="cursor-pointer" width={36} height={36} src="/notes.svg" alt="logo" />
+      <img
+        className="cursor-pointer"
+        width={36}
+        height={36}
+        src="/notes.svg"
+        alt="logo"
+      />
       <h1 className="font-semibold text-gray-600 text-xl">Noter</h1>
     </div>
     <div className="col-span-7 ">
@@ -15,8 +22,12 @@ const Navbar = () => (
       />
     </div>
     <div className="col-span-2 flex gap-3 justify-end ">
-      <GrAddCircle  size={32} className="float-right cursor-pointer"/>
-      <BiUserCircle size={32} className="float-right cursor-pointer" />
+      <GrAddCircle size={32} className="float-right cursor-pointer" />
+      <GrLogout
+        onClick={() => noterAuth.signOut()}
+        size={30}
+        className="float-right cursor-pointer"
+      />
     </div>
   </nav>
 );
