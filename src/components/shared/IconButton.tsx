d9190dc790d424bc479676b80182c53615efe0fe
Prasanna Kumar <prasannasrk07@gmail.com>
2021-04-23 22:10:54 +0530

    ♻️IconButton

---
+++ src/components/shared/IconButton.tsx
@@ -0,0 +1,14 @@
+interface Props {
+  onClick?: () => any;
+  children: JSX.Element;
+}
+
+const IconButton = ({ children, onClick }: Props) => {
+  return (
+    <button onClick={onClick} className="transition-all w-12 h-12 duration-200 p-3 hover:bg-gray-200 rounded-full">
+      {children}
+    </button>
+  );
+};
+
+export default IconButton;
